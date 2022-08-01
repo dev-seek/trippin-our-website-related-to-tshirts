03a592884e1e227e02ff1350357ea31db9b0afd7
manoj <manojkr20029835@gmail.com>
2022-08-01 07:36:33 +0530

    initial commit

---
+++ home/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HomeConfig(AppConfig): # copied app name manually and paste it in setting
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'home'
---
+++ home/views.py
@@ -0,0 +1,23 @@
+from unicodedata import name
+from django import http
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+    # return HttpResponse("this is home page")
+    
+
+def contact(request):
+    if request.method == "POST" :
+       name = request.POST.get('name')
+       email = request.POST.get('email')
+       phone = request.POST.get('phone')
+       desc = request.POST.get('desc')
+       contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+       contact.save()
+       messages.success(request, 'Thanks for choosing us ! we will contact you soon ')
+    return render(request,'index.html')
